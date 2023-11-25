a3475afaf8dcaf6bbe1c9593c80cd425a2fe09c8
SuperScary <wylanshoemaker@gmail.com>
2023-11-25 01:36:33 -0800

    removed bad imports

--- src/main/java/superscary/healthredux/config/ClientConfig.java
+++ src/main/java/superscary/healthredux/config/ClientConfig.java
@@ -1,20 +1,11 @@
 package superscary.healthredux.config;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 import superscary.healthredux.HealthReduxMod;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = HealthReduxMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientConfig
 {
